8747b33376375d614ca23e49b136d2a563b54d29
Анастасия Ческина <ncheskina13@gmail.com>
2024-03-14 15:52:48 +0300

    s

--- app/src/main/java/com/example/sportapp/Adapters/CustomCountdownTimer.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sportapp.Adapters
-
-class CustomCountdownTimer(private val millisInFuture:Long, private val countDownInterval:Long) {
-
-}
--- app/src/main/java/com/example/sportapp/TimerActivity.kt
+++ app/src/main/java/com/example/sportapp/TimerActivity.kt
@@ -3,12 +3,9 @@ package com.example.sportapp
 import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sportapp.Adapters.APIRequests
 import com.example.sportapp.Model.Workout
 import com.example.sportapp.databinding.ActivityTimerBinding
@@ -19,6 +16,7 @@ import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.awaitResponse
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.DecimalFormat
 
 class TimerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTimerBinding
@@ -29,6 +27,9 @@ class TimerActivity : AppCompatActivity() {
     private var listWorkout: List<Workout> = ArrayList<Workout>()
     private var allCount = 0
     private var currentCount = 0
+
+    private var currentTime = 0
+    private var timer: CountDownTimer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTimerBinding.inflate(layoutInflater)
@@ -48,20 +49,36 @@ class TimerActivity : AppCompatActivity() {
                     startActivity(intent)
                     finish()
                 } else {
-
                     counter++
                     initItems()
                 }
             }
-            btnPause.setOnClickListener {
-
-            }
-            btnPlay.setOnClickListener {
-
-            }
         }
     }
-
+    private fun startCountDownTimer(timeMillis: Long) { //в аргументах - время которое нужно отсчитать (мс)
+        timer?.cancel()
+        timer = object :
+            CountDownTimer(timeMillis, 1000) { //тут время и шаг: в данном случае 1с (1000 мс)
+            override fun onTick(timeM: Long) { //отсчет времени
+                val decimalFormat = DecimalFormat("00")
+                val timeFormat = "00:" + decimalFormat.format(timeM / 1000)
+                binding.time.text = timeFormat
+                binding.circularProgressBar.max = (timeM / 1000).toInt()
+                binding.circularProgressBar.progress = (timeM / 1000).toInt()
+            }
+            //когда время вышло
+            override fun onFinish() {
+                if (allCount == currentCount) {
+                    val intent = Intent(this@TimerActivity,EndTrainingActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    counter++
+                    initItems()
+                }
+            }
+        }.start()
+    }
     private fun getDataWorkout() {
         val api = Retrofit.Builder()
             .baseUrl(URL_API)
@@ -88,9 +105,10 @@ class TimerActivity : AppCompatActivity() {
             var selectedItem = listWorkout[counter]
             with(binding) {
                 nameWorkout.text = selectedItem.name
-
+                currentTime = selectedItem.durationInSec
                 allCount = listWorkout.count()
                 currentCount = counter + 1
+                startCountDownTimer((currentTime*1000).toLong())
             }
         }
     }
